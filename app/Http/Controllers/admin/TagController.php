29f8b1afa9d47a0298b83e41fbc6c821cbe5aff4
mdshibgatullah <mdshibgatullah94@gmail.com>
2026-08-08 14:37:19 +0600

    update

---
+++ app/Http/Controllers/admin/TagController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TagController extends Controller
+{
+    public function index(){
+        $tags = Tag::orderBy('created_at', 'DESC')->get();
+        return response()->json([
+            'status' => 200,
+            'data' => $tags
+        ]);
+    }
+
+
+    // store tag
+    public function store(Request $request){
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 400,
+                    'errors' => $validator->errors(),
+                ], 422);
+            }
+
+            $tag = Tag::create([
+                'name' => $request->name,
+                'status' => $request->status,
+            ]);
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'Tag created successfully',
+                'data' => $tag
+            ]);
+
+    }
+
+    // show tag
+    public function show($id){
+        $tag = Tag::findOrFail($id);
+       
+        if($tag == null){
+            return response()->json([
+                'status' => 404,
+                'message' => 'Tag not found',
+                'data' => []
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'data' => $tag
+        ]);
+
+    }
+
+
+    // update tag
+    public function update(Request $request, $id)
+    {
+        $tag = Tag::findOrFail($id);
+
+        if (!$tag) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Tag not found',
+                'data' => []
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $tag->update([
+            'name' => $request->name
+        ]);
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Tag updated successfully',
+            'data' => $tag
+        ], 200);
+    }
+
+
+    // destroy tag
+    public function destroy($id){
+        $tag = Tag::findOrFail($id);
+       
+        if($tag == null){
+            return response()->json([
+                'status' => 404,
+                'message' => 'Tag not found',
+                'data' => []
+            ], 404);
+        }
+
+        $tag->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Tag deleted successfully',
+            'data' => $tag
+        ], 200);
+    }
+}
